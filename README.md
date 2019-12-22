12964a290cbc9424ef2211b0d70fa2924c726b2b
melbarch <elbarchany@gmail.com>
2019-12-22 22:20:23 +0100

    Add more options for flexible usage
    Close #1

--- README.md
+++ README.md
@@ -8,18 +8,33 @@
 <img src="https://img.shields.io/npm/v/power-my-vscode.svg"/>
 
 
-I made this package to synchronize my VS Code setup across my computers (home's laptop, at work, fresh installations, ...).
-It powers VS Code with my preferred extensions and preferred theme.
+This package allows you to synchronize your VS Code Setup across your computers (Home's laptop, at work, fresh installations, ...).</br>
+If you change compiter or run a fresh installation, one command line will give give back powers to your VSCode.
 
 ## Usage :
-⚡ Zero config! one simple command line to upgrade VS Code : ⚡
-``` 
-npx power-my-vscode
-```
+Tool's command line offers multiple workflow possibilities :
 
+- ```npx power-my-vscode```</br>
+The package will install the default extensions defined [here](./.vs-extensions.json)
+- ```npx power-my-vscode -a```</br>
+This will lookup the current git username on your computer, try to lookup for the fork under its github profile then it will install extensions listed on : https://raw.githubusercontent.com/username/power-my-vscode/master/.vs-extensions.json
+- ```npx power-my-vscode -g <github_username>```</br>
+This is helpful if you want target someone's collection. It will do the same as the previous one but on https://raw.githubusercontent.com/github_username/power-my-vscode/master/.vs-extensions.json 
+- ```npx power-my-vscode -f <pah/to/json.file>```</br>
+Extensions list will be retrieved from that file
+- ```npx power-my-vscode -u <url_to_json_file>```</br>
+Will perfom installation from the list retrieved from that url (you might have some file with the list in you DotFiles repo for example)
+- Check out help command for more info : ```npx power-my-vscode -h```
 
+## Tip : Export your current extensions
+Your current extensions can be retrieved from VSCode CLI :
+````
+code --list-extensions 
+````
+Save the result of the command on your fork's file *.vs-extensions.json* or another file on your computer or over the internet depending on the workflow that suits you.
 
-List of extensions :
+## Default extensions
+List of available extensions by default :
 
 - **[Import cost](https://marketplace.visualstudio.com/items?itemName=wix.vscode-import-cost)**
 - **[Debugger for chrome](https://marketplace.visualstudio.com/items?itemName=msjsdiag.debugger-for-chrome)**
@@ -29,6 +44,8 @@ List of extensions :
 - **[Bracket Pair Colorizer 2](https://marketplace.visualstudio.com/items?itemName=CoenraadS.bracket-pair-colorizer-2)**
 - **[DotENV syntax highlighting](https://marketplace.visualstudio.com/items?itemName=mikestead.dotenv)**
 - **[Live server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer)**
+- **[XML Tools](https://marketplace.visualstudio.com/items?itemName=DotJoshJohnson.xml)**
+- **[PowerShell](https://marketplace.visualstudio.com/items?itemName=ms-vscode.PowerShell)**
 - My prefered theme : **[Nightowl](https://marketplace.visualstudio.com/items?itemName=sdras.night-owl)**
 
 ## License
--- index.js
+++ index.js
@@ -1,30 +1,16 @@
 #!/usr/bin/env node
 'use strict';
 
-const exec = require('child_process').exec;
-const promisify = require('util').promisify;
+const program = require('commander');
+const { exec } = require('child_process');
+const fs = require('fs');
+const { promisify } = require('util');
 const asyncExec = promisify(exec);
+
+const readFileAsync = promisify(fs.readFile);
 
 const chalk = require('chalk');
 const log = console.log;
-
-// TODO : move this into another file :
-const vsMarketPlaceItems = [
-  // Linters :
-  'dbaeumer.vscode-eslint',
-  'ms-vscode.vscode-typescript-tslint-plugin',
-
-  // utilities :
-  'wix.vscode-import-cost',
-  'msjsdiag.debugger-for-chrome',
-  'kisstkondoros.vscode-codemetrics',
-  'CoenraadS.bracket-pair-colorizer-2',
-  'mikestead.dotenv',
-  'ritwickdey.LiveServer',
-
-  // theme :
-  'sdras.night-owl'
-];
 
 let failures = 0;
 
@@ -35,24 +21,78 @@ const installExtension = async extentionId => {
   if (stdout) {
     log(stdout);
   }
-  if (stderr) {
-    log(chalk.bold.red('Error : ') + stderr);
+  if (stderr.includes('Failed Installing')) {
+    log(chalk.bold.red('[Error]') + stderr);
     failures++;
+  } else if (stderr) {
+    log(chalk.yellow('[Warning]') + stderr);
   }
 };
 
+const loadSettingsFromFile = async filePath => {
+  return JSON.parse(await readFileAsync(filePath, 'utf8'));
+};
+
 (async () => {
+  program
+    .option('-a, --auto', 'Detect current git user to retrieve his config')
+    .option(
+      '-g, --gitub-username <github_username>',
+      'github username to use to lookup the fork'
+    )
+    .option(
+      '-f, --file <filepath/to/file.json>',
+      'file system path to extensions json file'
+    )
+    .option('-u, --url <url-to-json-file>', 'url to extensions json file')
+    .parse(process.argv);
+
+  let settings;
+
+  if (program.gitubUsername) {
+    // settings = loadSettingsFromGithub(program.gitubUsername);
+    log(chalk.bold.red('Not yet implemented'));
+    process.exit(1);
+  } else if (program.file) {
+    settings = await loadSettingsFromFile(program.file);
+  } else if (program.url) {
+    // settings = await loadSettingsFromUrl(program.url);
+    log(chalk.bold.red('Not yet implemented'));
+    process.exit(1);
+  } else if (program.auto) {
+    /*
+    const {stdout, stderr} = await asyncExec('git config --global user.name');
+    let currentUser;
+    if (stdout) {
+      currentUser = stdout;
+    }
+    if (stderr) {
+      log(chalk.bold.red('Something went wrong! : ') + stderr);
+    }
+    settings = await loadSettingsFromUrl(getGithubSettingsUrl(currentUser));
+    */
+    log(chalk.bold.red('Not yet implemented'));
+    process.exit(1);
+  } else {
+    settings = await loadSettingsFromFile('.vs-extensions.json');
+  }
+
+  const vsMarketPlaceItems = settings.extensions;
   log(chalk.bold.green('Let\'s make your VS Code great again!'));
   const elementsCount = vsMarketPlaceItems.length;
-  // TODO : we can try to install only uninstalled extensions as code cli provide list of already installed extensions
   for (let index = 0; index < elementsCount; index++) {
     await installExtension(vsMarketPlaceItems[index]);
   }
   if (failures === 0) {
-    log(chalk.bold.green('Your VSCode have been given super powers successfully!'));
+    log(
+      chalk.bold.green('Your VSCode have been given super powers successfully!')
+    );
   } else {
-    log(chalk.yellowBright(
-      `Only ${elementsCount - failures} out of ${elementsCount} have been installed successfully!`
-    ));
+    log(
+      chalk.yellowBright(
+        `Only ${elementsCount -
+          failures} out of ${elementsCount} have been installed successfully!`
+      )
+    );
   }
 })();
